afb3f0470fe7a8dea694a6882068234d5e18cfc1
Jari Petays <jari.petays@iki.fi>
2025-08-20 11:22:44 +0300

    enable save button when Money is changed

--- StoneshardSaveEditor/MainForm.cs
+++ StoneshardSaveEditor/MainForm.cs
@@ -147,5 +147,10 @@ namespace StoneshardSaveEditor
                 this.additionalCharDataGroupBox.Enabled = false;
             }
         }
+
+        private void MoneyTextBox_TextChanged(object sender, EventArgs e)
+        {
+            EnableSaveButton(sender, e);
+        }
     }
 }
